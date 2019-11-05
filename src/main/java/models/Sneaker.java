ab6cfe8c4373adc15ec26dc5d3bda03d213616eb
val <valfragier@gmail.com>
2019-11-04 20:43:32 -0500

    Add Sneaker test

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -6,7 +6,7 @@ public class Sneaker {
     public static Sneaker getInstance() {
         return ourInstance;
     }
-    
+
     private int id;
     private String name;
     private String brand;
@@ -15,6 +15,62 @@ public class Sneaker {
     private int qty;
     private float price;
 
-    private Sneaker() {
+    Sneaker() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
     }
 }
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,4 +1,19 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SneakerTest {
+    @Test
+    public void setNameTest() {
+        // given (1)
+        String expected = "OZWEEGO";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
 }
